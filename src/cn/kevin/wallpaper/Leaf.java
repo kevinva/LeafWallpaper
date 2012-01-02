6d64acf52dd8ffcaff84983d8d4a808c26605613
kevinva <silverfox_222@163.com>
2012-01-02 21:07:26 +0800

    add touch event!

--- src/cn/kevin/wallpaper/Leaf.java
+++ src/cn/kevin/wallpaper/Leaf.java
@@ -6,29 +6,32 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
-public class Leaf {
-	
+public class Leaf {	
 	private float x;
 	private float y;
 	private float speedY;
 	private int heightBound;
 	private Bitmap bitmap;	
-
+	private float bounceOffset;
+	private boolean touched;
 
 	public Leaf(Bitmap source, int h){		
 		Random rand = new Random();		
-		speedY = 1 + rand.nextFloat() * 3;
+		
+		speedY = 1 + rand.nextFloat() * 3;		
 		float scaleRate = rand.nextFloat() * 0.5f;
 		if(scaleRate <= 0.01){
 			scaleRate = 0.1f;
 		}
 		int width = (int)(source.getWidth() * scaleRate);
 		int height = (int)(source.getHeight() * scaleRate);
-		bitmap = Bitmap.createScaledBitmap(source, width, height, true);
-		x = rand.nextFloat() * 400;
-		y = -bitmap.getHeight();
-		heightBound = h + bitmap.getHeight();
-		
+		bitmap = Bitmap.createScaledBitmap(source, width, height, true);		
+		x = rand.nextFloat() * 400;		
+		y = -bitmap.getHeight();		
+		heightBound = h + bitmap.getHeight();		
+		bounceOffset = 5.0f;
+		touched = false;
+
 	}
 
 	public float getX() {
@@ -53,15 +56,62 @@ public class Leaf {
 
 	public void setSpeedY(float speedY) {
 		this.speedY = speedY;
+	}	
+
+	public boolean isTouched(){
+		return touched;
 	}
 	
-	public void updateLeaf(Canvas canvas, Paint paint){
+	public void setTouched(boolean flag){
+		this.touched = flag;
+	}
+
+	public Bitmap getBitmap() {
+		return bitmap;
+	}
+
+	public void setBitmap(Bitmap bitmap) {
+		this.bitmap = bitmap;
+	}
+
+	public void drawLeaf(Canvas canvas, Paint paint){		
+		canvas.drawBitmap(bitmap, x, y, paint);
+	}	
+	
+	public void handleFalling(){
 		this.y += this.speedY;		
 		if(this.y >= this.heightBound){
 			this.y = -this.bitmap.getHeight();
 		}
+	}
+	
+	public void handleTouched(float touchX, float touchY){			
+		float centerX = this.x + this.bitmap.getWidth() / 2.0f;
+		float centerY = this.y + this.bitmap.getHeight() / 2.0f;
+
+		if(centerX <= touchX){
+			this.x -= this.bounceOffset;
+			if(centerY <= touchY){
+				this.y -= this.bounceOffset;
+			}else{
+				this.y += this.bounceOffset;
+			}
+			
+		}else{
+			this.x += this.bounceOffset;
+			if(centerY <= touchY){
+				this.y -= this.bounceOffset;
+			}else{
+				this.y += this.bounceOffset;
+			}
+		}
+
+		this.bounceOffset *= 0.8;
+		if(this.bounceOffset <= 1.0){
+			touched = false;
+			this.bounceOffset = 1.0f;
+		}	
 		
-		canvas.drawBitmap(bitmap, x, y, paint);
-	}	
+	}
 	
 }
--- src/cn/kevin/wallpaper/LeafFallingService.java
+++ src/cn/kevin/wallpaper/LeafFallingService.java
@@ -6,11 +6,13 @@ import java.util.Random;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Handler;
 import android.os.Message;
 import android.service.wallpaper.WallpaperService;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 
 public class LeafFallingService extends WallpaperService {
@@ -33,7 +35,7 @@ public class LeafFallingService extends WallpaperService {
 		super.onDestroy();
 	}
 	
-	private class WallpaperEngine extends Engine{
+	private class WallpaperEngine extends Engine implements OnGestureListener{
 		
 		private ArrayList<Leaf> leafList;
 		private Bitmap bitmap1;
@@ -44,6 +46,9 @@ public class LeafFallingService extends WallpaperService {
 		private int count;
 		private int heightOfCanvas;
 		private Random rand;
+		private GestureDetector detector;
+		private float touchX;
+		private float touchY;
 		
 		private static final int DRAW_MSG = 0;
 		private static final int MAX_SIZE = 100;
@@ -77,8 +82,11 @@ public class LeafFallingService extends WallpaperService {
 			this.paint.setAntiAlias(true);
 			this.count = -1;
 			this.rand = new Random();
-			
-		}
+			this.detector = new GestureDetector(this);
+			this.touchX = -1.0f;
+			this.touchY = -1.0f;
+			this.setTouchEventsEnabled(true);
+		}		
 		
 
 		@Override
@@ -151,11 +159,87 @@ public class LeafFallingService extends WallpaperService {
 			canvas.drawBitmap(backgroundBitmap, 0, 0, paint);
 			for(int i = 0; i < this.leafList.size(); i++){
 				Leaf l = this.leafList.get(i);
-				l.updateLeaf(canvas, paint);
+				if(l.isTouched()){
+					l.handleTouched(touchX, touchY);
+				}else{
+					l.handleFalling();
+				}		
+				l.drawLeaf(canvas, paint);
+				
 			}	
 			holder.unlockCanvasAndPost(canvas);
 			this.mHandler.sendEmptyMessageDelayed(DRAW_MSG, 15L);			
 			
+		}		
+
+
+		@Override
+		public void onTouchEvent(MotionEvent event) {
+			// TODO Auto-generated method stub
+			super.onTouchEvent(event);
+			this.detector.onTouchEvent(event);
+		}
+
+
+		@Override
+		public boolean onDown(MotionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("onDown");
+			
+			touchX = e.getX();
+			touchY = e.getY();	
+			for(int i = 0; i < this.leafList.size(); i++){
+				Leaf l = this.leafList.get(i);
+				float centerX = l.getX() + l.getBitmap().getWidth() / 2.0f;
+				float centerY = l.getY() + l.getBitmap().getHeight() / 2.0f;
+				
+				if(Math.abs(centerX - touchX) <= 80 && Math.abs(centerY - touchY) <= 80 && centerX != touchX){
+					l.setTouched(true);					
+				}				
+			}
+			
+			return true;
+		}
+
+
+		@Override
+		public void onShowPress(MotionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public boolean onSingleTapUp(MotionEvent e) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+
+		@Override
+		public boolean onScroll(MotionEvent e1, MotionEvent e2,
+				float distanceX, float distanceY) {
+			// TODO Auto-generated method stub
+			
+			
+			return false;
+		}
+
+
+		@Override
+		public void onLongPress(MotionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+			// TODO Auto-generated method stub
+			System.out.println("onFling");
+			
+			return false;
 		}				
 		
 	}
